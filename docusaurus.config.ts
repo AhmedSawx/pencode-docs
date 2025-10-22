7233903bb776f6ae0063871f217e0c000901d27a
AhmedSawx <50724805+AhmedSawx@users.noreply.github.com>
2025-10-22 21:15:34 +0300

    fixing website issue pt.1

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,13 +1,13 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'PenCode Documentation',
-  tagline: 'The creative coding environment for generative art.',
-  favicon: 'img/pencode-logo-dark.png',
+  title: "PenCode Documentation",
+  tagline: "The creative coding environment for generative art.",
+  favicon: "img/pencode-logo-dark.png",
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -15,42 +15,42 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: "https://ahmedsawx.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/pencode-docs/',
+  baseUrl: "/pencode-docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: '', // Usually your GitHub org/user name.
-  projectName: 'pencode-docs', // Usually your repo name.
+  organizationName: "ahmedsawx", // Usually your GitHub org/user name.
+  projectName: "pencode-docs", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: "throw",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
-          routeBasePath: '/', // Set this to '/' to make docs the root
+          sidebarPath: "./sidebars.ts",
+          routeBasePath: "/", // Set this to '/' to make docs the root
           versions: {
-            'alpha-1': {
-              label: 'Alpha 1',
+            "alpha-1": {
+              label: "Alpha 1",
             },
           },
         },
         blog: false,
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -61,64 +61,64 @@ const config: Config = {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: 'PenCode',
+      title: "PenCode",
       logo: {
-        alt: 'PenCode Logo',
-        src: 'img/pencode-logo-dark.png',
+        alt: "PenCode Logo",
+        src: "img/pencode-logo-dark.png",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Docs',
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Docs",
         },
         {
-          type: 'docsVersionDropdown',
-          position: 'right',
+          type: "docsVersionDropdown",
+          position: "right",
         },
         {
-          href: 'https://github.com/facebook/docusaurus', //TODO: replace with actual repo
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/facebook/docusaurus", //TODO: replace with actual repo
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Tutorial',
-              to: '/',
+              label: "Tutorial",
+              to: "/",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus",
             },
             {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus",
             },
             {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
+              label: "X",
+              href: "https://x.com/docusaurus",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus', //TODO: replace with actual repo
+              label: "GitHub",
+              href: "https://github.com/facebook/docusaurus", //TODO: replace with actual repo
             },
           ],
         },
@@ -128,7 +128,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
-      additionalLanguages: ['pencode'],
+      additionalLanguages: ["pencode"],
     },
   } satisfies Preset.ThemeConfig,
 };
